fa12e8f0eea1be03e00d403a75df717791ae9940
Abhishek Gobind Rao <abhishek.rao.4200@gmail.com>
2024-06-18 13:39:35 +0530

    responsive done

--- src/Components/Brands.jsx
+++ src/Components/Brands.jsx
@@ -38,19 +38,19 @@ export default function Brands() {
             <h1 className='text-white text-xl font-futura' style={{ fontWeight: 100 }}>YEAR 2024</h1>
           </div>
           <div className='heading'>
-            <p className='outlets text-8xl text-auto font-futura' style={{ fontWeight: 100 }}>12+ OUTLETS</p>
+            <p className='outlets text-6xl md:text-8xl font-futura' style={{ fontWeight: 100 }}>12+ OUTLETS</p>
           </div>
           <div className='bg-[#f2ea3e] h-1 mx-0'></div>
           <div className='sub-heading mx-2 my-2'>
-            <p className='texts1 text-4xl text-gray-500 font-futura' style={{ fontWeight: 100 }}>LAUNCHING MINDFUL</p>
-            <p className='texts2 text-4xl text-gray-500 font-futura' style={{ fontWeight: 100 }}>FOOD BRANDS</p>
-            <p className='texts3 text-4xl text-gray-500 font-futura' style={{ fontWeight: 100 }}>ACROSS DELHI NCR</p>
+            <p className='texts1 text-2xl md:text-4xl text-gray-500 font-futura' style={{ fontWeight: 100 }}>LAUNCHING MINDFUL</p>
+            <p className='texts2 text-2xl md:text-4xl text-gray-500 font-futura' style={{ fontWeight: 100 }}>FOOD BRANDS</p>
+            <p className='texts3 text-2xl md:text-4xl text-gray-500 font-futura' style={{ fontWeight: 100 }}>ACROSS DELHI NCR</p>
           </div>
           <div className='bg-orange-600 h-1 w-36 mx-2'></div>
         </div>
         
         <div className='right md:my-0 my-8'>
-          <img className='sm:h-[600px] sm:w-[700px] h-[300px] w-[400px]' src={img} alt="Brands" />
+          <img className='sm:h-[600px] sm:w-[700px] h-[300px] w-[400px] md:h-[600px] md:w-[700px]' src={img} alt="Brands" />
         </div>
       </div>
     </div>
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 export default function Footer() {
   return (
-    <>
-      <div className='all bg-[#e7eee6] p-12'>
-
-        <div className='heading flex justify-center items-center my-6 mb-'>
-            <h1 className='text-3xl tag font-futura '>CONTACT</h1>
-        </div>
-        <div className='sub-heading flex justify-center items-center flex-col mb-10'>
-<p className='text-[20px] font-futura'>Mail: connect@betrind.com</p>
-<p className='text-[20px] font-futura' >Address: 1st Floor, 07, Worldmark-1, Asset Area 11, Aerocity</p>
-        </div>
+    <div className='all bg-[#e7eee6] p-6 md:p-12'>
+      <div className='heading flex justify-center items-center my-4'>
+        <h1 className='text-2xl md:text-3xl tag font-futura'>CONTACT</h1>
       </div>
-    </>
-  )
+      <div className='sub-heading flex justify-center items-center flex-col mb-6 md:mb-10'>
+        <p className='text-base md:text-lg font-futura text-center'>
+          Mail: connect@betrind.com
+        </p>
+        <p className='text-base md:text-lg font-futura text-center'>
+          Address: 1st Floor, 07, Worldmark-1, Asset Area 11, Aerocity
+        </p>
+      </div>
+    </div>
+  );
 }
