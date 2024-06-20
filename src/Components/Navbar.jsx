ce8f720230d429b5d05de55b5709c9127ddacaf6
Abhishek Gobind Rao <abhishek.rao.4200@gmail.com>
2024-06-20 18:07:57 +0530

    nav bar highlight done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../images/BETR logo.svg'; // Import your logo
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+  const [activeLink, setActiveLink] = useState(null);
+  const [underlineStyle, setUnderlineStyle] = useState({});
+
   const scrollToSection = (id) => {
     const element = document.getElementById(id);
     if (element) {
+      const rect = element.getBoundingClientRect();
+      setUnderlineStyle({
+        width: `${rect.width}px`,
+        left: `${rect.left}px`
+      });
+      setActiveLink(id); // Set the active link based on the clicked section
       element.scrollIntoView({ behavior: 'smooth' });
     }
   };
@@ -18,13 +27,35 @@ export default function Navbar() {
             <img src={Logo} alt="BETR Logo" className='w-20 h-20' />
           </Link>
         </div>
-        <div className='navs my-2'>
+        <div className='navs my-2 relative'>
           <ul className='flex justify-center items-center md:flex-row flex-col '>
-            <li onClick={() => scrollToSection('about')} className='mx-8 text-gray-500 font-futura hover:cursor-pointer'>ABOUT.</li>
-            <li onClick={() => scrollToSection('brands')} className='mx-8 text-gray-500 font-futura hover:cursor-pointer'>BRANDS.</li>
-            <li onClick={() => scrollToSection('founders')} className='mx-8 text-gray-500 font-futura hover:cursor-pointer'>FOUNDERS.</li>
-            <li onClick={() => scrollToSection('contact')} className='mx-8 text-gray-500 font-futura hover:cursor-pointer'>CONTACT.</li>
+            <li
+              onClick={() => scrollToSection('about')}
+              className={`mx-8 text-gray-500 font-futura hover:cursor-pointer ${activeLink === 'about' ? 'text-yellow-500' : ''}`}
+            >
+              ABOUT.
+            </li>
+            <li
+              onClick={() => scrollToSection('brands')}
+              className={`mx-8 text-gray-500 font-futura hover:cursor-pointer ${activeLink === 'brands' ? 'text-yellow-500' : ''}`}
+            >
+              BRANDS.
+            </li>
+            <li
+              onClick={() => scrollToSection('founders')}
+              className={`mx-8 text-gray-500 font-futura hover:cursor-pointer ${activeLink === 'founders' ? 'text-yellow-500' : ''}`}
+            >
+              FOUNDERS.
+            </li>
+            <li
+              onClick={() => scrollToSection('contact')}
+              className={`mx-8 text-gray-500 font-futura hover:cursor-pointer ${activeLink === 'contact' ? 'text-yellow-500' : ''}`}
+            >
+              CONTACT.
+            </li>
           </ul>
+          {/* Underline */}
+         
         </div>
       </div>
     </>
